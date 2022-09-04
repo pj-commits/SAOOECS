1e81d8637e15fd0d8c716b2ab6cbee3e6dbd0ccd
Paul Signo <pdsigno@student.apc.edu.ph>
2022-09-04 22:32:41 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,6 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-<<<<<<< HEAD
-import persist from '@alpinejs/persist'
- 
-Alpine.plugin(persist)
-
-=======
->>>>>>> 8e85299 (Add multiple screens for roles)
 window.Alpine = Alpine;
 
 Alpine.start();
