387b1b2266b4a5c6dad85c856428945f0f955d5b
marcricahuerta <marckenneth100@gmail.com>
2022-09-05 19:37:15 +0800

    Sprint1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,13 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-<<<<<<< HEAD
         $this->call(LaratrustSeeder::class);
         // $this->call(UserSeeder::class);
 
      
 
-=======
->>>>>>> 827cb44 (ignore)
     }
 }
