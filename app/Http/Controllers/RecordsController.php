d82bbda937c4d35c14418bd942349b85111f8e3f
Paul Signo <pdsigno@student.apc.edu.ph>
2022-10-22 20:30:33 +0800

    Add records logic

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -2,16 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Form;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class RecordsController extends Controller
 {
-   public function index(){
-        //show pages for differnet users
-        if(Auth::user()->checkUSerType('Student|Staff|Professor')){
-            return view('_users.records');
-        }
-        return abort('404');
-   }
+    public function index()
+    {
+        /******************************************************************
+        *  Fetch forms: 
+        *  1. must be pending
+        *  2. must have forms.curr_approver = current user's dept/name + isHead 
+        *  (defines the forms place in process)
+        *  3. must have its staff_id == (approver)_staff_id // org = orgUser ID
+        *   
+        *       Forms ----> Staff and OrgUser, thats where we will check 
+        *       the id on forms 
+        *   
+        ********************************************************************/
+        
+        $approvedAndCancelled = Form::where('status', '=', 'Approved')
+            ->orWhere('status', '=', 'Cancelled')
+            ->where(function ($query) {
+                $user = auth()->user();
+                $staff = $user->userStaff;
+                $isHead = $staff->position === 'Head';
+
+                // LIST: id of orgs curr user belongs to
+                $getAuthOrgIdList = $user->studentOrg->pluck('id');
+
+                // LIST: orgUserId of curr user
+                $getAuthOrgUserIdList = $user->checkOrgUser->pluck('id');
+
+                $department = DB::table('departments')->find($staff->department_id);
+
+                if($user->checkUserType('Professor|Staff')){
+                    if($user->checkPosition('Adviser')){                            //  are you an adviser of an org?
+                        $query->whereIn('adviser_staff_id', $getAuthOrgUserIdList );//  form curr adviser_staff_id == your orgUser Id ?
+                        $query->whereIn('organization_id', $getAuthOrgIdList);      //  form is part of curr user's org ? 
+                        $query->where('curr_approver', 'Adviser');                  //  form curr_approver == adviser ?
+                    }
+    
+                    if($department->name === 'Student Activities Office' && $isHead ){
+                        $query->where('sao_staff_id', $staff->id);
+                        $query->where('curr_approver', 'SAO');
+                    }
+                    
+                    if($department->name === 'Academic Services' && $isHead){
+                        $query->where('acadserv_staff_id', $staff->id);
+                        $query->where('curr_approver', 'Academic Services');
+                    }
+                    
+                    if($department->name === 'Finance Office'  && $isHead){
+                        $query->where('finance_staff_id', $staff->id);
+                        $query->where('curr_approver', 'Finance');      
+                    }
+                }elseif($user->checkUserType('Student')){
+                    $query->whereIn('organization_id', $getAuthOrgIdList); 
+                }   
+                
+                
+            })
+           ->paginate(10);
+
+        return view('_users.records', compact('approvedAndCancelled'));
+    }
 }
